89211325b8eee3bf3c8e9534670e1604cf606b37
Luca <luca.campus03@gmail.com>
2026-04-28 15:58:50 +0200

    enable rendering in random policy test and adjust episode count

--- part1/test_random_policy.py
+++ part1/test_random_policy.py
@@ -11,7 +11,7 @@
 import gymnasium as gym
 
 def main():
-    render = False
+    render = True
 
     if render:
         env = gym.make('Hopper-v4', render_mode='human')
@@ -20,15 +20,17 @@ def main():
     print('State space:', env.observation_space)  # state-space
     print('Action space:', env.action_space)  # action-space
 
-    n_episodes = 50
-
+    n_episodes = 50000
+    a=1
+    
     for ep in range(n_episodes):  
         done = False
-        state, info = env.reset()  # Reset environment to initial state
+        if(a==1):
+            state, info = env.reset()  # Reset environment to initial state
+            a = 0
 
         while not done:  # Until the episode is over
             action = env.action_space.sample()  # Sample random action
-
             state, reward, terminated, truncated, _ = env.step(action)  # Step the simulator to the next timestep
             done = terminated or truncated
 
