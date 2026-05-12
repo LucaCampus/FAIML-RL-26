5f8cf54724416111d74d87a6665cd972ee2c1534
Antonio <148047332+Antoniovetrone03@users.noreply.github.com>
2026-05-12 17:46:36 +0200

    Modified all

--- part2/train_sb3.py
+++ part2/train_sb3.py
@@ -4,9 +4,28 @@ from collections import deque
 import gymnasium as gym
 import numpy as np
 import panda_gym  # type: ignore[import-not-found]
-from stable_baselines3 import DDPG
+from stable_baselines3 import SAC, PPO
 from rand_wrapper import RandomizationWrapper
+import wandb
 
+wandb.init(
+
+    project="faiml-rl",
+
+    config={
+
+        "algorithm": args.algo,
+        "env": args.env_type,
+        "timesteps": args.timesteps,
+        "sampling_strategy": args.sampling_strategy,
+        "learning_rate": 3e-4,
+
+    },
+    sync_tensorboard=True,
+    monitor_gym=True,
+    save_code=True,
+
+)
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="Train SAC on PandaPush-v3")
@@ -30,6 +49,13 @@ def parse_args() -> argparse.Namespace:
         default=500_000,
         help="Number of training timesteps",
     )
+    parser.add_argument(
+        "--algorithm",
+        type=str,
+        default="ppo",
+        choices=["sac", "ppo"],
+        help="RL algorithm to use for training",
+    )
     return parser.parse_args()
 
 
@@ -44,9 +70,17 @@ def main() -> None:
     )
 
     #TODO: add randomization wrapper here
+    # if args.sampling_strategy != "none":
+    #     env = RandomizationWrapper(env, args.sampling_strategy)
     #TODO: create model and train it
-    save_name = f"sac_push_{args.sampling_strategy}_{args.env_type}_{args.timesteps // 1000}k"
+    if args.algorithm == "sac":
+        model = SAC("MultiInputPolicy", env)
+    elif args.algorithm == "ppo":
+        model = PPO("MultiInputPolicy", env)
+    model.learn(total_timesteps=args.timesteps)
+    save_name = f"{args.algorithm}_{args.sampling_strategy}_{args.env_type}_{args.timesteps // 1000}k"
     # TODO: model.save(save_name)
+
 
 
 if __name__ == "__main__":
